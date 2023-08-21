3ce3d70d8d84149313cdd87567ff3bc22a015ec0
vlad.shanin <shanin.vladyslav@gmail.com>
2023-08-21 16:33:21 +0300

    Set up docker compose demo

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # docker-workshop
 
 ```bash
-docker build . --tag demo:0.1
-docker run -p 8402:8402 -v $(pwd):/app -d -it demo:0.1
+docker compose up --build
+docker compose down
 ```
--- src/app.py
+++ src/app.py
@@ -1,9 +1,18 @@
+from redis import Redis
 from fastapi import FastAPI
 
 
 app = FastAPI()
+r = Redis(host='redis', port=6379)
 
 
 @app.get("/")
 async def root():
-    return {"message": "Surprise!"}
+    if 'counter' not in [k.decode() for k in r.keys()]:
+        counter = 1
+    else:
+        counter = int(r['counter'].decode()) + 1
+
+    r['counter'] = counter
+
+    return {"message": f"Surprise {counter}!"}
